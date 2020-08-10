39765c28f03b35a365a57753cf72c9401f000fb4
Ogaga <lordoadjaro@gmail.com>
2020-08-10 18:24:35 +0100

    fixed social media links

---
+++ src/app/services/posts/posts.service.ts
@@ -0,0 +1,139 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor() { }
+}
+const posts = [
+
+  {
+    "author":"OGAGA ADJARO",
+    "postTime": "dd/mm/yyyy",
+    "title":"",
+    "body":    {
+      "h1":"",
+      "p1":"",
+      "p2":"",
+      "p3":"",
+      "h2":"",
+      "p4":"",
+      "p6":"",
+      "p7":"",
+      "p8":"",
+    }
+  },
+  {
+    "author":"OGAGA ADJARO",
+    "postTime": "dd/mm/yyyy",
+    "title":"",
+    "body":    {
+      "h1":"",
+      "p1":"",
+      "p2":"",
+      "p3":"",
+      "h2":"",
+      "p4":"",
+      "p6":"",
+      "p7":"",
+      "p8":"",
+    }
+  },
+  {
+    "author":"OGAGA ADJARO",
+    "postTime": "dd/mm/yyyy",
+    "title":"",
+    "body":    {
+      "h1":"",
+      "p1":"",
+      "p2":"",
+      "p3":"",
+      "h2":"",
+      "p4":"",
+      "p6":"",
+      "p7":"",
+      "p8":"",
+    }
+  },
+  {
+    "author":"OGAGA ADJARO",
+    "postTime": "dd/mm/yyyy",
+    "title":"",
+    "body":    {
+      "h1":"",
+      "p1":"",
+      "p2":"",
+      "p3":"",
+      "h2":"",
+      "p4":"",
+      "p6":"",
+      "p7":"",
+      "p8":"",
+    }
+  },
+  {
+    "author":"OGAGA ADJARO",
+    "postTime": "dd/mm/yyyy",
+    "title":"",
+    "body":    {
+      "h1":"",
+      "p1":"",
+      "p2":"",
+      "p3":"",
+      "h2":"",
+      "p4":"",
+      "p6":"",
+      "p7":"",
+      "p8":"",
+    }
+  },
+  {
+    "author":"OGAGA ADJARO",
+    "postTime": "dd/mm/yyyy",
+    "title":"",
+    "body":    {
+      "h1":"",
+      "p1":"",
+      "p2":"",
+      "p3":"",
+      "h2":"",
+      "p4":"",
+      "p6":"",
+      "p7":"",
+      "p8":"",
+    }
+  },
+  {
+    "author":"OGAGA ADJARO",
+    "postTime": "dd/mm/yyyy",
+    "title":"",
+    "body":    {
+      "h1":"",
+      "p1":"",
+      "p2":"",
+      "p3":"",
+      "h2":"",
+      "p4":"",
+      "p6":"",
+      "p7":"",
+      "p8":"",
+    }
+  },{
+    "author":"OGAGA ADJARO",
+    "postTime": "dd/mm/yyyy",
+    "title":"",
+    "body":    {
+      "h1":"",
+      "p1":"",
+      "p2":"",
+      "p3":"",
+      "h2":"",
+      "p4":"",
+      "p6":"",
+      "p7":"",
+      "p8":"",
+    }
+  }
+]
